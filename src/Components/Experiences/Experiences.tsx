693d2fa249ed053d9b6bdf610ffc17917338e556
Hazem Kawas <hazem.k.4488@gmail.com>
2023-03-27 09:24:33 +0200

    exper

--- src/Components/Experiences/Experiences.tsx
+++ src/Components/Experiences/Experiences.tsx
@@ -1,269 +1,311 @@
-import { Box, ImageList, ImageListItem, ImageListItemBar, SxProps, Typography} from '@mui/material'
-import { blueGrey } from '@mui/material/colors';
-import { useState } from 'react';
-import './Experiences.css'
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-
-
-
+import { Box, SxProps, Typography } from "@mui/material";
+import { blueGrey } from "@mui/material/colors";
+import { useState } from "react";
+import "./Experiences.css";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, EffectCoverflow } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import NM_LOGO_WHITE from "../../img/work/NM_LOGO_WHITE.png";
+import VEOLIA from "../../img/work/VEOLIA.jpg";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 const Experiences = () => {
- const [isWorkSecOpen, setIsWorkSecOpen] = useState(false)
- const [isEduSecOpen, setIsEduSecOpen] = useState(false)
- const [activeSec, setActiveSec] = useState(false)
+  const [isWorkSecOpen, setIsWorkSecOpen] = useState(false);
+  const [isEduSecOpen, setIsEduSecOpen] = useState(false);
+  const [activeSec, setActiveSec] = useState(false);
 
-
+  const pagination = {
+    clickable: true,
+    // renderBullet: function (index: number, className: string) {
+    //   // return '<span class="' + className + '">' + (index + 1) + "</span>";
+    //   return <p></p>;
+    // },
+  };
 
   return (
-    <Box sx={LayoutBox} className='fadein'>
-      {/* <Box className= {isEduSecOpen ? 'animation' : 'noAnimation' }>
-      <img   src={eduIcon} alt="" />
-    </Box> */}
-      {/* <Box className= {isWorkSecOpen ? 'workAnimation' : 'noAnimation' }>
-        <img  className='workImage' src="https://kinsta.com/wp-content/uploads/2020/04/code-review-tools.png" alt="" />
-      </Box> */}
-     
-    <Box sx={BoxStyle} className={isWorkSecOpen ? 'innerBox' : 'noBorderAnimation' } >
-      {isWorkSecOpen ? 
-       <Box className="education">
-        <Box >
-          <HighlightOffIcon 
-          sx={closeIcon}
-          onClick={() => {
-            isWorkSecOpen ? setIsWorkSecOpen(false) : setIsWorkSecOpen(true)
-            activeSec ? setActiveSec(false) : setActiveSec(true);
-          }} 
-          />
-        </Box>
-       <ImageList sx={imageListBox}>
-        {workData.map((item, index) => (
-        <ImageListItem key={index}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-            className="edu-image"
-          />
-            <p className="mainTitle">{item.title}</p>
-            <ImageListItemBar
-              title={item.field}
-              subtitle={item.date}
-          />
-        </ImageListItem>
-     ))}
-   </ImageList>
-     </Box> : 
-     <Box >
-        <Typography 
-        sx={activeSec ? mainTextsActive : mainTexts}
-        onClick={() => {
-          isEduSecOpen ? setIsEduSecOpen(false) : setIsEduSecOpen(true);
-          activeSec ? setActiveSec(false) : setActiveSec(true);
-        }}>
-        Education
-        </Typography>
-     </Box>
-        
-    }
-    
+    <Box sx={LayoutBox} className="fadein">
+      <Box
+        sx={BoxStyle}
+        className={isWorkSecOpen ? "innerBox" : "noBorderAnimation"}
+      >
+        {isWorkSecOpen ? (
+          <Box className="education">
+            <Box>
+              <HighlightOffIcon
+                sx={closeIcon}
+                onClick={() => {
+                  isWorkSecOpen
+                    ? setIsWorkSecOpen(false)
+                    : setIsWorkSecOpen(true);
+                  activeSec ? setActiveSec(false) : setActiveSec(true);
+                }}
+              />
+            </Box>
+            <Box>
+              <Swiper
+                spaceBetween={10}
+                initialSlide={0}
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 10,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                pagination={pagination}
+                modules={[EffectCoverflow, Pagination]}
+                className="slide"
+              >
+                {workData.map((img) => (
+                  <SwiperSlide key={img.id} className="slider">
+                    <Box className="slide-content">
+                      <Box className="exp-image">
+                        <img src={img.img} alt="" className="exp-image" />
+                      </Box>
+                      <Box className="">
+                        <h1>{img.title}</h1>
+                        <p>{img.field}</p>
+                        <p>{img.date}</p>
+                      </Box>
+                    </Box>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </Box>
+          </Box>
+        ) : (
+          <Box>
+            <Typography
+              sx={activeSec ? mainTextsActive : mainTexts}
+              onClick={() => {
+                isEduSecOpen ? setIsEduSecOpen(false) : setIsEduSecOpen(true);
+                activeSec ? setActiveSec(false) : setActiveSec(true);
+              }}
+            >
+              Education
+            </Typography>
+          </Box>
+        )}
+      </Box>
+      <Box
+        sx={BoxStyle}
+        className={isEduSecOpen ? "innerBox" : "noBorderAnimation"}
+      >
+        {isEduSecOpen ? (
+          <Box className="education">
+            <Box>
+              <HighlightOffIcon
+                sx={closeIcon}
+                onClick={() => {
+                  isEduSecOpen ? setIsEduSecOpen(false) : setIsEduSecOpen(true);
+                  activeSec ? setActiveSec(false) : setActiveSec(true);
+                }}
+              />
+            </Box>
+            <Box>
+              <Swiper
+                spaceBetween={10}
+                initialSlide={0}
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                pagination={pagination}
+                modules={[EffectCoverflow, Pagination]}
+                className="slide"
+              >
+                {educationData.map((img) => (
+                  <SwiperSlide key={img.id} className="slider">
+                    <Box className="slide-content">
+                      <Box className="exp-image">
+                        <img src={img.img} alt="" className="exp-image" />
+                      </Box>
+                      <Box className="">
+                        <h1>{img.title}</h1>
+                        <p>{img.field}</p>
+                        <p>{img.date}</p>
+                      </Box>
+                    </Box>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </Box>
+          </Box>
+        ) : (
+          <Box>
+            <Typography
+              sx={activeSec ? mainTextsActive : mainTexts}
+              onClick={() => {
+                isWorkSecOpen
+                  ? setIsWorkSecOpen(false)
+                  : setIsWorkSecOpen(true);
+                activeSec ? setActiveSec(false) : setActiveSec(true);
+              }}
+            >
+              Work
+            </Typography>
+          </Box>
+        )}
+      </Box>
     </Box>
-    <Box sx={BoxStyle} className={isEduSecOpen ? 'innerBox' : 'noBorderAnimation' }>
-      {isEduSecOpen ? 
-      <Box className="education" >
-        <Box>
-          <HighlightOffIcon  
-          sx={closeIcon} 
-          onClick={() => {
-            isEduSecOpen ? setIsEduSecOpen(false) : setIsEduSecOpen(true);
-            activeSec ? setActiveSec(false) : setActiveSec(true);
-          }} 
-          />
-        </Box>
-        <ImageList sx={imageListBox}>
-        {educationData.map((item, index) => (
-          <ImageListItem key={index}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-              className="edu-image"
-            />
-            <p className="mainTitle">{item.title}</p>
-            <ImageListItemBar
-              title={item.field}
-              subtitle={item.date}
-              sx={itemsDetails}
-              // actionIcon={
-              //   <IconButton
-              //     sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-              //     aria-label={`info about ${item.title}`}
-              //   >
-              //     <InfoIcon />
-              //   </IconButton>
-              // }
-            />
-          </ImageListItem>
-
-        ))}
-      </ImageList>
-        </Box> : 
-        <Box  >
-          <Typography 
-            sx= {activeSec ? mainTextsActive : mainTexts} 
-            className='mainText'
-            onClick={() => {
-            isWorkSecOpen ? setIsWorkSecOpen(false) : setIsWorkSecOpen(true)
-            activeSec ? setActiveSec(false) : setActiveSec(true);
-            }}>
-            Work
-          </Typography>
-      </Box> 
-    }
-    </Box>
-    </Box>
-  )
-}
+  );
+};
 
 const LayoutBox: SxProps = {
-  width: { xs: '100%', md: '100%',lg: '100%', xl: '80%'},
+  width: { xs: "100%", md: "100%", lg: "80%", xl: "80%" },
   // paddingLeft: { xs: '0', md: '0',lg: '8em' },
-  margin: { xs: 'auto', md: '0',lg: 'auto' },
-  height: { xs: '80vh', md: '100vh',lg: '100vh', xl: '100vh'},
-  display: 'flex',
-  flexDirection: {xs: 'column', md: 'column', lg: 'row'},
-  alignItems: 'center',
-  justifyContent: {xs: 'space-around', md: 'column', lg: 'space-around', xl: 'space-around'},
+  margin: { xs: "auto", md: "0", lg: "auto" },
+  height: { xs: "100vh", md: "100vh", lg: "100%", xl: "100vh" },
+  display: "flex",
+  flexDirection: { xs: "column", md: "column", lg: "row" },
+  alignItems: "center",
+  justifyContent: {
+    xs: "space-around",
+    md: "column",
+    lg: "space-around",
+    xl: "space-around",
+  },
   textColor: blueGrey[900],
-  zIndex: '2'
-}
+  zIndex: "2",
+  marginTop: { xs: "5em", md: "0em", lg: "0", xl: "0" },
+};
 const BoxStyle: SxProps = {
-  width: { xs: '100%', md: '100%',lg: '50%', xl: '50%' },
-  display: 'flex',
-  alignItems: 'center',
+  width: { xs: "100%", md: "100%", lg: "50%", xl: "50%" },
+  display: "flex",
+  alignItems: "center",
   justifyContent: "center",
+
   // height: '20vh',
-}
+};
 const mainTexts: SxProps = {
-  cursor: 'pointer',
-  fontSize: { xs: '3em', md: '3em',lg: '5em' },
-  webkitTextStroke: '0.2px #eceff0',
-  fontFamily: 'Gentium Book Plus, serif',  
-  color: 'rgba(32, 216, 207, 0.993)',
-  zIndex: '10',
-  p: '1em',
-  borderRadius:'.4em',
-  minWidth: { xs: '250px', md: '80%',lg: '350px' },
-  display: 'flex',
-  alignItems: 'center',
+  cursor: "pointer",
+  fontSize: { xs: "3em", md: "3em", lg: "5em" },
+  webkitTextStroke: "0.2px #eceff0",
+  fontFamily: "Gentium Book Plus, serif",
+  color: "rgba(32, 216, 207, 0.993)",
+  zIndex: "10",
+  p: "1em",
+  borderRadius: ".4em",
+  minWidth: { xs: "250px", md: "250px", lg: "300px" },
+  display: "flex",
+  alignItems: "center",
   justifyContent: "center",
-  backgroundColor: 'rgba(17, 62, 80, 0.493)',
-  boxShadow:  '2px 2px 10px 5px  rgba(33, 253, 253, 0.5)',
-  '&:hover': {
-    boxShadow:  '2px 2px 15px 10px  rgba(33, 253, 169, 0.5)',
-  }
-}
+  backgroundColor: "rgba(17, 62, 80, 0.493)",
+  boxShadow: "2px 2px 10px 5px  rgba(33, 253, 253, 0.5)",
+  backdropFilter: "blur(20px)",
+  "&:hover": {
+    boxShadow: "2px 2px 15px 10px  rgba(33, 253, 169, 0.5)",
+  },
+};
 const mainTextsActive: SxProps = {
-  cursor: 'pointer',
-  fontSize: { xs: '3em', md: '3em',lg: '3em', xl: '3em' },
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  position: 'relative',
-  fontFamily: 'Gentium Book Plus, serif',  
-  color: 'rgba(10, 181, 248, 0.8)',
-  zIndex: '10',
-  padding: '1em',
-  borderRadius:'.4em',
-  boxShadow:  '2px 2px 150px 50px  rgba(33, 253, 253, 0.5)',
-  backgroundColor: 'rgba(17, 62, 80, 0.493)',
-  minWidth: { xs: '250px', md: '350px', lg: '350px' },
-}
+  cursor: "pointer",
+  fontSize: { xs: "3em", md: "3em", lg: "3em", xl: "3em" },
+  display: { xs: "none", md: "flex", lg: "flex", xl: "flex" },
+  alignItems: "center",
+  justifyContent: "center",
+  position: "relative",
+  fontFamily: "Gentium Book Plus, serif",
+  color: "rgba(10, 181, 248, 0.8)",
+  zIndex: "10",
+  padding: { xs: ".2em", md: "1em", lg: "1em", xl: "1em" },
+  borderRadius: ".4em",
+  boxShadow: "2px 2px 100px 1px  rgba(33, 253, 253, 0.5)",
+  backgroundColor: "rgba(17, 62, 80, 0.493)",
+  minWidth: { xs: "250px", md: "350px", lg: "350px" },
+  backdropFilter: "blur(10px)",
+  "&:hover": {
+    boxShadow: "2px 2px 15px 10px  rgba(33, 253, 169, 0.5)",
+  },
+};
 const imageListBox: SxProps = {
-  width: { xs: '350px', md: '80%',lg: '800px',xl: '600px' },
-  height: { xs: '340px', md: '80%',lg: '800px', xl: '600px' },
-  borderRadius: '2em',
-}
+  width: { xs: "350px", md: "80%", lg: "800px", xl: "600px" },
+  height: { xs: "340px", md: "80%", lg: "800px", xl: "600px" },
+  borderRadius: "2em",
+};
 const closeIcon: SxProps = {
-  width: { xs: '50px', md: '',lg: '',xl: '60px' },
-  height: { xs: '50px', md: '',lg: '', xl: '60px' },
-  position: 'absolute',
-  fontSize: '4em',
-  top: '15px',
-  right: '15px',
-  zIndex: '8',
-  fill:"#c83349",
-  cursor: 'pointer',
-}
+  width: { xs: "50px", md: "60px", lg: "60px", xl: "60px" },
+  height: { xs: "50px", md: "60px", lg: "60px", xl: "60px" },
+  position: "absolute",
+  fontSize: "4em",
+  top: "15px",
+  right: "15px",
+  zIndex: "8",
+  fill: "#c83349",
+  cursor: "pointer",
+};
 const itemsDetails: SxProps = {
-  '&:hover': {
-    backgroundColor: 'rgba(35, 160, 233, 0.21)',
-    borderRadius: '.5em'
-  }
-}
-
-
-
-
+  "&:hover": {
+    backgroundColor: "rgba(35, 160, 233, 0.21)",
+    borderRadius: ".5em",
+  },
+};
 
 const educationData = [
   {
-    img: 'https://storage.googleapis.com/ares-profile-pictures/hd/medieinstitutet-7f3bdbdf1dd4d7fd4246ab8b72ff30b5_hd.jpg',
-    title: 'Medieinstitutet',
-    field: 'Front-End Devloper',
+    id: 1,
+    img: "https://storage.googleapis.com/ares-profile-pictures/hd/medieinstitutet-7f3bdbdf1dd4d7fd4246ab8b72ff30b5_hd.jpg",
+    title: "Medieinstitutet",
+    field: "Front-End Developer",
     date: "Sept 2021 - May 2023",
     featured: true,
   },
   {
-    img: 'https://yellow.place/file/image/thumb/0/0/1304/djmrjyskgpdsmlya.jpg',
-    title: 'Goteborg tekniska college',
-    date: "Aug 2018 - May 2019",
-    field: 'Elinstallation',
+    id: 2,
+    img: "https://storage.googleapis.com/ares-profile-pictures/hd/medieinstitutet-7f3bdbdf1dd4d7fd4246ab8b72ff30b5_hd.jpg",
+    title: "Medieinstitutet",
+    field: "Webbutvecklare inom e-handel",
+    date: "Sep 2020 – jan 2021",
   },
   {
-    img: 'https://bloggcampusmolndal.files.wordpress.com/2017/12/campusmolndal_logo.jpg',
-    title: 'Campus mölndal',
-    field: 'Front-End Devloper',
+    id: 3,
+    img: "https://bloggcampusmolndal.files.wordpress.com/2017/12/campusmolndal_logo.jpg",
+    title: "Campus mölndal",
+    field: "Front-End Developer",
     date: "Aug 2019 – may 2020",
   },
   {
-    img: 'https://storage.googleapis.com/ares-profile-pictures/hd/medieinstitutet-7f3bdbdf1dd4d7fd4246ab8b72ff30b5_hd.jpg',
-    title: 'Medieinstitutet',
-    field: 'Webbutvecklare inom e-handel',
-    date: "Sep 2020 – jan 2021",
+    id: 4,
+    img: "https://yellow.place/file/image/thumb/0/0/1304/djmrjyskgpdsmlya.jpg",
+    title: "Goteborg tekniska college",
+    date: "Aug 2018 - May 2019",
+    field: "Elinstallation",
   },
-  
 ];
 const workData = [
   {
-    img: 'https://yt3.ggpht.com/ZNCwA8zByeJJAw7gzknCTGjbNIHokCksg275U8vsv4QB17LizNtnlhb2itZXivhQbPTGpH1J3aI=s900-c-k-c0x00ffffff-no-rj',
-    title: 'VEOLIA',
-    field: 'INTERNSHIP-Electrical installation',
+    id: 1,
+    img: `${NM_LOGO_WHITE}`,
+    title: "Nordic Morning",
+    field: "INTERNSHIP-Front-End Developer",
+    date: "January-2023 - may-2023",
+  },
+  {
+    id: 2,
+    img: `${NM_LOGO_WHITE}`,
+    title: "Nordic Morning",
+    field: "INTERNSHIP-Front-End Developer",
+    date: "September-2022 - November-2022",
+  },
+  {
+    id: 3,
+    img: ` ${VEOLIA}`,
+    title: "VEOLIA",
+    field: "INTERNSHIP-Electrical installation",
     date: "Mars-2019",
-    rows: 2,
-    cols: 2,
-    featured: true,
   },
-  {
-    img: 'https://teflsearch.com/sites/default/files/im%20academy.jpg',
-    title: 'IM Academy',
-    field: 'Summer activity supervisor',
-    date: 'May-2015 – Aug-2015'
-  },
-  {
-    img: 'https://yellow.place/file/image/cover/0/0/2671/lpdqfrhaagwnsolp.jpg',
-    title: 'HANIN CAFÉ',
-    field: 'Owner',
-    date: "Oct-2011 – Oct-2014",
-  },
-  {
-    img: 'http://pharaon-group.com/data/uploads/sawa-logo.png',
-    title: 'SAWA',
-    field: 'Customer support',
-    date: "Jan-2011 – Oct-2011",
-    cols: 2,
-  },
-  
 ];
-export default Experiences
+export default Experiences;
